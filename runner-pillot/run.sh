579bf70690a6ac18671060ea7bc01d4c73646594
0xaungkon <0xaungkon@gmail.com>
2025-07-25 18:41:49 +0000

    feature: Initalizing installer

---
+++ runner-pillot/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <github_repo_url> <github_token>"
+    exit 1
+    fi
+
+    REPO_URL="$1"
+    GITHUB_TOKEN="$2"
+    REPO_NAME=runner_$(basename "$REPO_URL" | tr '[:upper:]' '[:lower:]')
+
+    docker stop $REPO_NAME
+    docker rm $REPO_NAME
+    docker run -d --restart=always \
+      -e GITHUB_REPO_URL=$REPO_URL \
+        -e GITHUB_RUNNER_TOKEN=$GITHUB_TOKEN \
+          --name $REPO_NAME \
+            github-runner:2.327.0
