e8a793fe9dae36e0cc3e956f1808c13c33375b19
0xaungkon <aungkonmalakar@gmail.com>
2025-10-24 13:14:31 +0600

    feat: add random joke endpoint and jokes.json file

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routers import meta
 from inc.config import is_dev
+import json, random
 
 app = FastAPI()
 
@@ -16,10 +17,19 @@ async def startup_event():
 from routers import auth, common
 from routers import runner_instance
 from routers import system
+import random
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(common.router, prefix="/common", tags=["common"])
 app.include_router(system.router, prefix="/system", tags=["system"])
+
+
+with open("jokes.json", "r") as f:
+	_jokes = json.load(f)
+
+@app.get("/", tags=["root"])
+async def root():
+	return {"joke": random.choice(_jokes)}
 
 if is_dev():
 	app.include_router(meta.router, prefix="/meta", tags=["meta"])
